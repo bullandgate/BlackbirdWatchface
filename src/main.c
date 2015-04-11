66e4cbdf390f3bf0b88166560ed8d00f43b5ffeb
Tim Frost <bullandgate@gmail.com>
2015-04-11 12:07:48 +0100

    Update
    Changed the fonts and added the date.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,9 @@
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static GFont s_time_font;
+static GFont s_date_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -13,6 +15,7 @@ static void update_time() {
 
   // Create a long-lived buffer
   static char buffer[] = "00:00";
+  static char date_buffer[] = "Xxxxxxxxx 00";
 
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
@@ -22,44 +25,61 @@ static void update_time() {
     //Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
+  
+  	strftime(date_buffer, sizeof(date_buffer), "%B %e", tick_time);
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_date_layer, date_buffer);
 }
 
 static void main_window_load(Window *window) {
   // Create GBitmap, then set to created BitmapLayer
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_CROW);
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BLACKBIRD);
   s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(6, 0, 139, 50));
+  s_time_layer = text_layer_create(GRect(0, 4, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_EXTRUDE_48));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BLACK_32));
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
 
+  // Create date TextLayer
+  s_date_layer = text_layer_create(GRect(0, 39, 139, 50));
+  text_layer_set_background_color(s_date_layer, GColorClear);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_text(s_date_layer, "%B %e");
+  // Create GFont
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_MEDIUM_16));
+  // Apply to TextLayer
+  text_layer_set_font(s_date_layer, s_date_font);
+  
   // Improve the layout to be more like a watchface
-  // text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   
   // Make sure the time is displayed from the start
   update_time();
+
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
   // Unload GFont
   fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_date_font);
   // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
   // Destroy BitmapLayer
